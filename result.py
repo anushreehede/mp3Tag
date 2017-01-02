9ecce5af25cdb841f4e26bfea5f8a5d31d4f36bc
Anushree Hede <shuttler22@gmail.com>
2017-01-02 23:16:26 +0530

    Error in retrieving artist name

--- result.py
+++ result.py
@@ -1,13 +1,18 @@
 def results(results, num):
-	#print "hello"
+	print "hello"
 	for score, rid, title, artist in results:
-		if num == "0":
-			#print 0
-			return score  
-		if num == "1":
-			#print 1
+		if num == 0:
+			score = score*100
+			newscore = str(score)
+			#newscore = "{:.2f}".format(score) 
+			print newscore
+			return newscore  
+		if num == 1:
+			print rid
 			return rid
-		if num == "2":
+		if num == 2:
+			print title
 			return title
-		if num == "3":
+		if num == 3:
+			#print artist
 			return artist
--- search.cpp
+++ search.cpp
@@ -21,13 +21,25 @@ int main(int argc, char* argv[])
         exit(EXIT_FAILURE);
     }
 
-    PyObject *pName, *pModule, *pFunc, *pArgs, *pResults, *pName1, *pModule1, *pFunc1, *pArgs1;
+    PyObject *pName, *pModule, *pFunc, *pArgs, *pResults, *pName1, *pModule1, *pFunc1, *pArgs1, *pValue;
 	Py_Initialize();
+    
+    PyRun_SimpleString("import sys");
+    PyRun_SimpleString("sys.path.append(\".\")"); 
 	pName = PyString_FromString("acoustid");
     pModule = PyImport_Import(pName);
     Py_DECREF(pName);
-
+    if(pModule == NULL)
+    {
+        PyErr_Print();
+        exit(-1);
+    }
     pFunc = PyObject_GetAttrString(pModule, "match");
+    if(pFunc == NULL)
+    {
+        printf("ERROR getting func attribute");
+        exit(-1);
+    }
     pArgs = PyTuple_New(2);
 
     PyTuple_SetItem(pArgs, 0, PyString_FromString(apikey));
@@ -35,12 +47,10 @@ int main(int argc, char* argv[])
     //PyTuple_SetItem(pArgs, 2, PyString_FromString(META));
 
     pResults = PyObject_CallObject(pFunc, pArgs);
-    
-    std::cout<<"^&\n";
 
     char *score = NULL; char *rid = NULL; char *title = NULL; char *artist = NULL;
 
-    pName1 = PyString_FromString("hello");
+    pName1 = PyString_FromString("result");
     pModule1 = PyImport_Import(pName1);
     Py_DECREF(pName1);
     if(pModule1 == NULL)
@@ -48,34 +58,40 @@ int main(int argc, char* argv[])
         PyErr_Print();
         exit(-1);
     }
-    std::cout<<"^&\n";
-    
     pFunc1 = PyObject_GetAttrString(pModule1, "results");
     if(pFunc1 == NULL)
     {
-        printf("ERROR getting func attribute");
+        PyErr_Print();
         exit(-1);
     }
-    std::cout<<"^&\n";
     pArgs1 = PyTuple_New(2);
-    std::cout<<"^&\n";
     PyTuple_SetItem(pArgs1, 0, pResults);
-    //std::cout<<"#$%\n";
+    //std::cout<<"@#$%^&\n";
     for(int i=0; i<4; ++i)
     {
     	switch(i)
     	{
     		case 0: PyTuple_SetItem(pArgs1, 1, PyLong_FromLong(0));
-    		        score = PyString_AsString(PyObject_CallObject(pFunc1, pArgs1)); 
+    		        pValue = PyObject_CallObject(pFunc1, pArgs1); 
+                    score = PyString_AsString(pValue);
+                    /*if(score == NULL)
+                        std::cout<<"yoyo\n";*/
+                    //std::cout<<score<<"\n";
     		        break;// call get_results, send 0 as param and store return in score
     		case 1: PyTuple_SetItem(pArgs1, 1, PyLong_FromLong(1));
     		        rid = PyString_AsString(PyObject_CallObject(pFunc1, pArgs1)); 
+                    /*if(rid == NULL)
+                        std::cout<<"yoyo\n";*/
     		        break;// call get_results, send 1 as param and store return in rid
     		case 2: PyTuple_SetItem(pArgs1, 1, PyLong_FromLong(2));
     		        title = PyString_AsString(PyObject_CallObject(pFunc1, pArgs1)); 
+                    /*if(title == NULL)
+                        std::cout<<"yoyo\n";*/
     		        break;// call get_results, send 2 as param and store return in title
     		case 3: PyTuple_SetItem(pArgs1, 1, PyLong_FromLong(3));
     		        artist = PyString_AsString(PyObject_CallObject(pFunc1, pArgs1)); 
+                    if(artist == NULL)
+                        std::cout<<"null\n";
     		        break;// call get_results, send 3 as param and store return in artist
     		default: break;
     	}
